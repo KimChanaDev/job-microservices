ab3e393742bb0e496eb31c598b2195d181917e4f
KimChanaDev <chanaweestr@gmail.com>
2025-09-10 21:58:52 +0700

    feat(auth): add CreateUserInput DTO and implement user creation with password hashing

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,13 +1,30 @@
 import { PrismaService } from '@app/prisma';
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client/auth/index.js';
+import { hash } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
 
-    constructor(private readonly prismaService: PrismaService) {}
+    constructor(private readonly prismaService: PrismaService) { }
+
+    async createUser(userData: Prisma.UserCreateInput) {
+        const user = await this.prismaService.user.create({
+            data: {
+                ...userData,
+                password: await hash(userData.password, 10),
+            },
+        });
+        return user;
+    }
 
     async getUsers() {
         return this.prismaService.user.findMany();
     }
 
+    async getUser(args: Prisma.UserWhereUniqueInput) {
+        return this.prismaService.user.findUniqueOrThrow({
+            where: args,
+        });
+    }
 }
